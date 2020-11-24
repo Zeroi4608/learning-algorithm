31de1e61689a187fcef4e837655cf87e9649f53d
子曰 <991305750@qq.com>
2020-11-24 16:36:59 +0800

    update

---
+++ one-question-per-day/README.md
@@ -0,0 +1,44 @@
+## 每日一题（刷题狂魔组）
+
+⚠️每日打卡链接：https://jinshuju.net/f/PX4VfW
+
+<font color=Darkgray>未开始</font>
+<font color=LightGreen>进行中</font>
+<font color=Cyan>已完成</font>  
+
+### **Day1** 
+>题目：爬楼梯  
+>状态：<font color=Cyan>已完成</font>  
+>编码：leetcode-70  
+>难度：简单  
+>地址：https://leetcode-cn.com/problems/climbing-stairs/
+
+### **Day2** 
+>题目：加一  
+>状态：<font color=Cyan>已完成</font>     
+>编码：leetcode-66  
+>难度：简单  
+>地址：https://leetcode-cn.com/problems/plus-one/
+
+### **Day3** 
+>题目：两数之和  
+>状态：<font color=Cyan>已完成</font>     
+>编码：leetcode-1  
+>难度：简单  
+>地址：https://leetcode-cn.com/problems/two-sum/  
+
+### **Day4** 
+>题目：两两交换链表中的节点  
+>状态：<font color=Cyan>已完成</font>  
+>编码：leetcode-24  
+>难度：**中等**  
+>地址：https://leetcode-cn.com/problems/swap-nodes-in-pairs/  
+
+### **Day5** 
+>题目：合并两个有序链表  
+>状态：<font color=Cyan>已完成</font>  
+>编码：leetcode-21  
+>难度：简单  
+>地址：https://leetcode-cn.com/problems/merge-two-sorted-lists/  
+
+
---
+++ one-question-per-day/day4-swap-paris.js
@@ -0,0 +1,79 @@
+var swapPairs = function(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    const newHead = head.next;
+    head.next = swapPairs(newHead.next);
+    newHead.next = head;
+    return newHead;
+};
+
+// //定义节点
+// function ListNode(val) {
+//   this.val = val;
+//   this.next = null;
+// }
+
+// //定义链表
+// //链表中有头结点和一些方法
+// function LinkedList() {
+//   this.head = new ListNode("head");
+//   this.find = find;
+//   this.insert = insert;
+//   this.remove = remove;
+//   this.show = show;
+//   this.findFront = findFront;
+// }
+
+// //定义一些方法
+// //查找节点
+// function find(item) {
+//   var curNode = this.head;
+//   while (curNode.val != item) {
+//     curNode = curNode.next;
+//   }
+//   return curNode; //找到就返回结点数据，没有就返回null
+// }
+
+// //插入节点
+// function insert(newElement, item) {
+//   //插入的节点，和位置
+//   var newNode = new ListNode(newElement);
+//   var curNode = this.find(item);
+//   var nextNode = curNode.next;
+//   // newNode.next = curNode.next;
+//   curNode.next = newNode;
+//   newNode.next = nextNode;
+// }
+// //找到前节点
+// function findFront(item) {
+//   var curNode = this.head;
+//   if (curNode.next.val != item) {
+//     curNode = curNode.next;
+//   }
+//   return curNode;
+// }
+// //删除节点
+// function remove(item) {
+//   var frontNode = this.findFront(item);
+//   var curNode = this.find(item);
+//   frontNode.next = curNode.next;
+// }
+// //遍历节点
+// function show() {
+//   var curNode = this.head,
+//     result = "";
+//   while (curNode.next != null) {
+//     result += curNode.next.val;
+//     curNode = curNode.next;
+//   }
+//   return result;
+// }
+
+// var list = new LinkedList();
+// list.insert("a", "head");
+// list.insert("b", "a");
+// list.insert("c", "b");
+// console.log(list.show());
+// list.remove("b");
+// console.log(list.show());
